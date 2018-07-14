1feb5db06631388b44070e4e7057c36ba1ccc8f0
Kyle Schaffrick <kyle@raidi.us>
2018-07-14 05:45:37 -0700

    Add support for display offset.
    This allows the `Display` interface's idea of the display's origin to be
    positioned correctly when the display module wires the physical rows and
    columns of the display onto the row and com driver lines in an offset
    fashion.
    
    The row offset is compensated by `SetDisplayOffset` command which is
    built-in to the hardware. Column offset is handled in software.

--- src/display.rs
+++ src/display.rs
@@ -15,6 +15,7 @@ where
 {
     iface: DI,
     display_size: PixelCoord,
+    display_offset: PixelCoord,
     persistent_config: Option<PersistentConfig>,
 }
 
@@ -23,19 +24,21 @@ where
     DI: interface::DisplayInterface,
 {
     /// Construct a new display driver for a display of a particular size.
-    pub fn new(iface: DI, display_size: PixelCoord) -> Self {
+    pub fn new(iface: DI, display_size: PixelCoord, display_offset: PixelCoord) -> Self {
         if false
             || display_size.0 > NUM_PIXEL_COLS as i16
             || display_size.1 > NUM_PIXEL_ROWS as i16
-            || display_size.0 > NUM_PIXEL_COLS as i16
-            || display_size.1 > NUM_PIXEL_ROWS as i16
+            || display_offset.0 + display_size.0 > NUM_PIXEL_COLS as i16
+            || display_offset.1 + display_size.1 > NUM_PIXEL_ROWS as i16
             || display_size.0.mod_euc(4) != 0
+            || display_offset.0.mod_euc(4) != 0
         {
-            panic!("Display size not supported by SSD1322.");
+            panic!("Display size or column offset not supported by SSD1322.");
         }
         Display {
             iface: iface,
             display_size: display_size,
+            display_offset: display_offset,
             persistent_config: None,
         }
     }
@@ -47,7 +50,7 @@ where
         config.send(&mut self.iface)?;
         self.persistent_config = Some(config.persistent_config);
         Command::SetMuxRatio(self.display_size.1 as u8).send(&mut self.iface)?;
-        Command::SetDisplayOffset(0).send(&mut self.iface)?;
+        Command::SetDisplayOffset(self.display_offset.1 as u8).send(&mut self.iface)?;
         Command::SetStartLine(0).send(&mut self.iface)?;
         self.persistent_config.as_ref().unwrap().send(
             &mut self.iface,
@@ -72,17 +75,29 @@ where
     /// Construct a rectangular region onto which to draw image data. The region start and end
     /// horizontal coordinates must be divisible by 4, because pixels can only be addressed by
     /// column (groups of 4), not individually. The region rectangle must also be within the
-    /// viewable area of the display buffer.
+    /// viewable area of the display buffer, where the viewable area includes all 128 rows to
+    /// support vertical panning.
     pub fn region<'di>(
         &'di mut self,
         upper_left: PixelCoord,
         lower_right: PixelCoord,
     ) -> Result<Region<'di, DI>, ()> {
+        // The row fields are bounds-checked against the chip's maximum supported row rather than
+        // the display size, because the display supports vertical scrolling by adding an offset to
+        // the memory address that corresponds to row 0 (`SetStartLine` command). This feature
+        // makes it possible to "pan" displays with fewer rows up and down over the entire 128
+        // buffer rows. So, allow users to draw in that area even if it's currently hidden.
+        //
+        // The chip does not have any such panning support for buffer column addresses outside of
+        // the display's viewable area, so even though the chip allows data to be written there, it
+        // is probbly an error because it can never be read back and can never be visible on the
+        // display. So, check column values against the display size and do not allow drawing
+        // outside them.
         if false
             || upper_left.0 > self.display_size.0
             || lower_right.0 > self.display_size.0
-            || upper_left.1 > self.display_size.1
-            || lower_right.1 > self.display_size.1
+            || upper_left.1 > NUM_PIXEL_ROWS as i16
+            || lower_right.1 > NUM_PIXEL_ROWS as i16
             || upper_left.0 >= lower_right.0
             || upper_left.1 >= lower_right.1
             || upper_left.0.mod_euc(4) != 0
@@ -90,7 +105,12 @@ where
         {
             return Err(());
         }
-        Ok(Region::new(&mut self.iface, upper_left, lower_right))
+
+        // The column offset only is added to the pixel coordinates of the region. The row offset
+        // is handled by the display driver itself using the `SetDisplayOffset` command.
+        let ul = PixelCoord(upper_left.0 + self.display_offset.0, upper_left.1);
+        let lr = PixelCoord(lower_right.0 + self.display_offset.0, lower_right.1);
+        Ok(Region::new(&mut self.iface, ul, lr))
     }
 }
 
@@ -157,7 +177,7 @@ mod tests {
     #[test]
     fn init_defaults() {
         let di = TestSpyInterface::new();
-        let mut disp = Display::new(di.split(), Px(128, 64));
+        let mut disp = Display::new(di.split(), Px(128, 64), Px(0, 0));
         let cfg = Config::new(ComScanDirection::RowZeroLast, ComLayout::DualProgressive);
         disp.init(cfg).unwrap();
         #[cfg_attr(rustfmt, rustfmt_skip)]
@@ -176,7 +196,7 @@ mod tests {
     #[test]
     fn init_many_options() {
         let di = TestSpyInterface::new();
-        let mut disp = Display::new(di.split(), Px(256, 128));
+        let mut disp = Display::new(di.split(), Px(256, 128), Px(0, 0));
         let cfg = Config::new(ComScanDirection::RowZeroLast, ComLayout::DualProgressive)
             .contrast_current(160)
             .phase_lengths(5, 14)
@@ -207,9 +227,28 @@ mod tests {
     }
 
     #[test]
+    fn init_row_offset() {
+        let di = TestSpyInterface::new();
+        let mut disp = Display::new(di.split(), Px(128, 64), Px(0, 32));
+        let cfg = Config::new(ComScanDirection::RowZeroLast, ComLayout::DualProgressive);
+        disp.init(cfg).unwrap();
+        #[cfg_attr(rustfmt, rustfmt_skip)]
+        di.check_multi(sends!(
+            0xAE, // sleep enable
+            0xA4, // display blank
+            0xCA, [63], // mux ratio 64 lines
+            0xA2, [32], // display offset 32
+            0xA1, [0], // start line 0
+            0xA0, [0b00010100, 0b00010001], // remapping
+            0xAF, // sleep disable
+            0xA6 // display normal
+        ));
+    }
+
+    #[test]
     fn region_build() {
         let di = TestSpyInterface::new();
-        let mut disp = Display::new(di.split(), Px(128, 64));
+        let mut disp = Display::new(di.split(), Px(128, 64), Px(0, 0));
         let cfg = Config::new(ComScanDirection::RowZeroLast, ComLayout::DualProgressive);
         disp.init(cfg).unwrap();
 
@@ -227,14 +266,17 @@ mod tests {
 
         // Column out of range.
         assert!(disp.region(Px(124, 4), Px(132, 6)).is_err());
-        // Row out of range.
+        // Row out of display range but not buffer range: not an error! The display can "pan"
+        // vertically to see this region.
+        assert!(disp.region(Px(12, 60), Px(20, 128)).is_ok());
+        // Row out of buffer range: error.
         assert!(disp.region(Px(4, 60), Px(20, 130)).is_err());
     }
 
     #[test]
     fn region_draw_packed() {
         let mut di = TestSpyInterface::new();
-        let mut disp = Display::new(di.split(), Px(128, 64));
+        let mut disp = Display::new(di.split(), Px(128, 64), Px(0, 0));
         let cfg = Config::new(ComScanDirection::RowZeroLast, ComLayout::DualProgressive);
         disp.init(cfg).unwrap();
         di.clear();
@@ -254,5 +296,25 @@ mod tests {
             assert!(region.draw_packed(&[0xDE, 0xAD, 0xBE]).is_err());
             assert!(region.draw_packed(&[0xDE, 0xAD, 0xBE, 0xEF, 0xAA]).is_err());
         }
+    }
+
+    #[test]
+    fn region_draw_packed_display_column_offset() {
+        let mut di = TestSpyInterface::new();
+        let mut disp = Display::new(di.split(), Px(128, 64), Px(64, 0));
+        let cfg = Config::new(ComScanDirection::RowZeroLast, ComLayout::DualProgressive);
+        disp.init(cfg).unwrap();
+        di.clear();
+        {
+            let mut region = disp.region(Px(0, 10), Px(4, 12)).unwrap();
+            region.draw_packed(&[0xDE, 0xAD, 0xBE, 0xEF]).unwrap();
+        }
+        #[cfg_attr(rustfmt, rustfmt_skip)]
+        di.check_multi(sends!(
+            0x15, [16, 16],
+            0x75, [10, 11],
+            0x5C, [0xDE, 0xAD, 0xBE, 0xEF]
+        ));
+        di.clear();
     }
 }
